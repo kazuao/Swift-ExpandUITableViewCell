40f0229df49785d1ccb35f7ba0cd37c76c663551
fores_KazunoriAoki <k-aoki@fores.jp>
2021-09-15 09:16:30 +0900

    initial commit

---
+++ ExpandUITableViewCell/ExpandUITableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpandUITableViewCell
+//
+//  Created by k-aoki on 2021/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
